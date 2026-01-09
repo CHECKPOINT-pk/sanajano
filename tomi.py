ba53cc7979a646590ac0849fe246c7a27f0ab16a
CHECKPOINT-pk <alimehr1234566@gmail.com>
2026-01-09 18:52:05 +0500

    Update tomi.py

--- tomi.py
+++ tomi.py
@@ -273,13 +273,13 @@ if random.choice([True, False]):
     fbav = f"{fb_major}.0.0.{random.randint(10, 99)}.{random.randint(100, 999)}"
     fban = random.choice(fban_types)
 
-        Ua = (
-    f"Mozilla/5.0 (Linux; Android {ver}; {model} Build/QP1A.190711.020; wv) "
-    f"AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 "
-    f"Chrome/{chrome_ver} Mobile Safari/537.36 "
-    f"[FBAN/{fban};FBAV/{fbav};FBDV/{model};"
-    f"FBMF/{brand};FBBD/{brand};FBSV/{ver};FBOP/{random.randint(1,5)};FBCR/{random.choice(['Jazz','Zong','Telenor','Ufone','Airtel','Jio','MTN'])}]"
-)
+    Ua = (
+        f"Mozilla/5.0 (Linux; Android {ver}; {model} Build/QP1A.190711.020; wv) "
+        f"AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 "
+        f"Chrome/{chrome_ver} Mobile Safari/537.36 "
+        f"[FBAN/{fban};FBAV/{fbav};FBDV/{model};"
+        f"FBMF/{brand};FBBD/{brand};FBSV/{ver};FBOP/{random.randint(1,5)};FBCR/{random.choice(['Jazz','Zong','Telenor','Ufone','Airtel','Jio','MTN'])}]"
+    )
     else:
         # -------- iPhone UA --------
         model, ios_ver = random.choice(iphone_models)
